379f9cf18ba7e646655f26ceaeed98febc1aa189
Alex-Nagy <nagyal3x@gmail.com>
2022-01-12 12:27:42 +0100

    cars

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import { useState } from "react";
 import "./App.css";
-// landin, home, about. 3 gomb. stringek
 
 // ---------------------------------------------------------
+const cars = [
+  {
+    brand: "BMW",
+    model: "i3",
+    fuel: "electric",
+  },
+  {
+    brand: "Volkswagen",
+    model: "Golf",
+    fuel: "Gas",
+  },
+  {
+    brand: "Tesla",
+    model: "S",
+    fuel: "electric",
+  },
+  {
+    brand: "Honda",
+    model: "Accord",
+    fuel: "Gas",
+  },
+];
+
 
 function App() {
   const [pageName, setPageName] = useState("Home");
-
   const [shouldShowLanding, setShouldShowLanding] = useState(true);
+  const [myInputValue, setMyInputValue] = useState("");
+
+  console.log("render");
+  console.log(myInputValue);
 
   return (
     <div className="App">
@@ -30,8 +55,30 @@ function App() {
       </main>
 
       <div>
-        {pageName === "Landing" || <div>This is not the Landing component</div>}
+        {pageName !== "Landing" && <div>This is not the Landing component</div>}
       </div>
+
+      <hr />
+
+      <input
+        value={myInputValue}
+        onChange={(e) => setMyInputValue(e.target.value)}
+      ></input>
+
+      {myInputValue.length < 3 ? (
+        <input type="submit" disabled />
+      ) : (
+        <input type="submit" />
+      )}
+      {myInputValue.length < 3 && <div>Min 3 char!</div>}
+
+      <ul>
+        { cars.filter(name => name.brand.includes({myInputValue})).map(car => (
+          <li key={car.model}>
+            {car.brand} - {car.model}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
